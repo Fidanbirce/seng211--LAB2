e34dd8d15d41baae6bcd2778aa65a6a806571b5e
bircefidan <zeynepbircefidan@gmail.com>
2025-10-17 14:59:32 +0300

    init

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public void displayInfo(){
+        System.out.println(year+" "+brand+" "+model);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
+    public static void main(String[] args){
+        Car c1=new Car();
+        c1.brand="Toyota";
+        c1.model="Corolla";
+        c1.year =2020;
+        c1.displayInfo();
+
+    }
 
 }
