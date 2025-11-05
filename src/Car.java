9df22ad4f8046edf493eba1f1a144d954ce86e55
bircefidan <zeynepbircefidan@gmail.com>
2025-11-05 19:31:52 +0300

    init

--- src/Car.java
+++ src/Car.java
@@ -8,4 +8,5 @@ public class Car {
         System.out.println(year+" "+brand+" "+model);
         enginethis.showPower();
     }
+
 }
--- src/Enginethis.java
+++ src/Enginethis.java
@@ -6,4 +6,5 @@ public class Enginethis {
     public void showPower(){
         System.out.println("Engine Power:"+horsePower+"HP");
     }
+
 }
